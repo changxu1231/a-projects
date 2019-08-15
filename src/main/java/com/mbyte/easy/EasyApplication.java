3ca6957d112d7a26bdf84355aefaaa873ee8d8cd
humingx <1204465968@qq.com>
2019-08-15 09:56:27 +0800

    cx修改404页面

--- src/main/java/com/mbyte/easy/EasyApplication.java
+++ src/main/java/com/mbyte/easy/EasyApplication.java
@@ -3,10 +3,10 @@ package com.mbyte.easy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
 /**
+
  * SpringBoot启动类
- * @author  王震
+ * @author  常旭
  */
 @SpringBootApplication
 @EnableScheduling
--- src/main/java/com/mbyte/easy/admin/controller/WeixinUserController.java
+++ src/main/java/com/mbyte/easy/admin/controller/WeixinUserController.java
@@ -9,17 +9,13 @@ import com.mbyte.easy.common.controller.BaseController;
 import com.mbyte.easy.common.web.AjaxResult;
 import com.mbyte.easy.util.FileUtil;
 import com.mbyte.easy.util.PageInfo;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.util.List;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 /**
 * <p>
